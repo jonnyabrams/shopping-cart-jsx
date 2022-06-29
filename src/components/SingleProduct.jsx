3a5d5a6fa715498f3e62ad7130822b0e0c8c2b94
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-29 13:41:27 +0100

    Add img, price, delivery & rating to SingleProduct card

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,10 +1,23 @@
 import { Card } from "react-bootstrap"
+import Rating from './Rating'
 
 const SingleProduct = ({ product }) => {
   return (
     <div className="products">
       <Card>
         <Card.Img variant='top' src={product.image} alt={product.name} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>£ {product.price.split(".")[0]}</span>
+            { product.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 days delivery</div>
+            )}
+            <Rating rating={product.ratings} />
+          </Card.Subtitle>
+        </Card.Body>
       </Card>
     </div>
   )
