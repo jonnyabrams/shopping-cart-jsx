16ac0fbe8492dbfa15c80085548e955b03766242
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-29 10:26:58 +0100

    Add dropdown to Header (with placeholder number 1)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import { Link } from 'react-router-dom'
-import { Container, FormControl, Navbar } from 'react-bootstrap'
+import { Container, FormControl, Navbar, Dropdown, Nav, Badge, CarouselItem } from 'react-bootstrap'
+import { FaShoppingCart } from "react-icons/fa"
 
 const Header = () => {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
-        <Container>
-          <Navbar.Brand>
-            <a href="">Shopping Cart</a>
-          </Navbar.Brand>
-          <Navbar.Text className="search">
-            <FormControl style={{ width: 500 }} placeholder="Search for a product..." className="m-auto" />
-          </Navbar.Text>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href="">Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl style={{ width: 500 }} placeholder="Search for a product..." className="m-auto" />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{1}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
   )
 }
 
