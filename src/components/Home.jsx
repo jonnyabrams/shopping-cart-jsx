d2197f22ef7a06ce637d0219b3922e12ac90da42
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-29 12:05:39 +0100

    Display all product names on Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,18 @@
 import { CartState } from "../context/Context"
 
 const Home = () => {
-
   // extra destructuring to get just the products from the state - just { state } would give us cart too
   const { state: { products } } = CartState()
   
 
   return (
-    <div>Home</div>
+    <div className="product_container">
+      {
+        products.map((product) => {
+          return <span>{ product.name }</span>
+        })
+      }
+    </div>
   )
 }
 
